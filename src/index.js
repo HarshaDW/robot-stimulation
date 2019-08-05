ddc1fe9ac17d0ba3c3c8b019525f91db856f3d26
HarshaDWijendra <harsha.d.wijendra@live.com>
2019-08-05 08:03:54 +0530

    Pushing local changes

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/style.scss';
+import { Game } from './containers/Game';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Main = props => (
+    <BrowserRouter>
+    <div>
+    <Route path="/" exact component={Game} />
+</div>
+</BrowserRouter>
+);
+
+ReactDOM.render(<Main />, document.getElementById('root'));
